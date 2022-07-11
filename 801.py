576bd70cd5f32cc1d82526910d5bd7cf9f74dd9e
Ritesh Rohilla <65576858+riteshrohilla@users.noreply.github.com>
2022-07-11 18:42:32 +0530

    Create 801.py

---
+++ 801.py
@@ -0,0 +1,6 @@
+a = [1, 0, 0, 1, 1, 1, 0]
+b = [bool(x) for x in a]
+# print(b)
+# print(a)
+# print(any(b)) # if any is true it will return true
+print(all(b)) # if all are true it will return true
